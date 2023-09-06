2de4e7e9b0ebed49a51f26005f178a3dd166d83e
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-09-06 03:38:28 +0100

    Error Handling and changing core functionality

--- main.js
+++ main.js
@@ -70,109 +70,150 @@ const callInput = document.querySelector("#callInput");
 const answerButton = document.querySelector("#answerButton");
 const hangupButton = document.querySelector("#hangupButton");
 const remoteVideo = document.querySelector("#remoteVideo");
+const tipCont = document.querySelector(".tip");
+const tipMessage = tipCont.querySelector(".message");
 
-webcamButton.addEventListener("click", async function () {
-	localStream = await navigator.mediaDevices.getUserMedia({
-		video: 1,
-		audio: 1,
-	});
+console.log(tipCont, tipMessage);
 
-	remoteStream = new MediaStream();
-	localStream.getTracks().forEach((track) => {
-		pc.addTrack(track, localStream);
-	});
+const state = {
+	online: true,
+	error: "",
+};
 
-	pc.addEventListener("ontrack", (e) => {
-		e.streams[0].getTracks().forEach((track) => {
-			remoteStream.addTrack(track);
+const openWebcam = async function () {
+	try {
+		localStream = await navigator.mediaDevices.getUserMedia({
+			video: 1,
+			audio: 1,
 		});
-	});
+		remoteStream = new MediaStream();
+		localStream.getTracks().forEach((track) => {
+			pc.addTrack(track, localStream);
+		});
 
-	webCamVideo.srcObject = localStream;
-	remoteVideo.srcObject = remoteStream;
-});
-
-callButton.addEventListener("click", async function () {
-	const callsCollection = collection(firestore, "calls");
-	const callDoc = doc(callsCollection);
-
-	// ...
-
-	const callDocRef = doc(callsCollection, callDoc.id);
-	const offerCandidates = collection(callDocRef, "offerCandidates");
-	const answerCandidates = collection(callDocRef, "answerCandidates");
-
-	callInput.value = callDocRef.id;
-
-	// Add icecandidate event listener
-	pc.addEventListener("icecandidate", async (event) => {
-		console.log(event.candidate);
-		if (event.candidate) {
-			// Handling the ice candidate
-			await addDoc(offerCandidates, event.candidate.toJSON());
-		}
-	});
-
-	const offerDescription = await pc.createOffer();
-	await pc.setLocalDescription(offerDescription);
-
-	const offer = {
-		sdp: offerDescription.sdp,
-		type: offerDescription.type,
-	};
-
-	// Store the offer in Firestore
-	await setDoc(callDocRef, { offer }); // Use setDoc to store the offer as an object
-
-	// Listen for changes in Firestore document
-	onSnapshot(callDocRef, (snapshot) => {
-		// Listen to callDocRef instead of callsCollection
-		const data = snapshot.data();
-		console.log(data);
-
-		if (!pc.currentRemoteDescription && data?.answer) {
-			const answerDescription = new RTCSessionDescription(data.answer);
-			pc.setRemoteDescription(answerDescription);
-		}
-
-		// Handle answerCandidates as you were doing
-		onSnapshot(answerCandidates, (snapshot) => {
-			snapshot.docChanges().forEach((change) => {
-				if (change.type === "added") {
-					const candidate = new RTCIceCandidate(change.doc.data());
-					pc.addIceCandidate(candidate);
-				}
+		pc.addEventListener("ontrack", (e) => {
+			e.streams[0].getTracks().forEach((track) => {
+				remoteStream.addTrack(track);
 			});
 		});
+
+		webCamVideo.srcObject = localStream;
+		remoteVideo.srcObject = remoteStream;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const timeout = function (s) {
+	return new Promise(function (_, reject) {
+		setTimeout(function () {
+			reject(
+				new Error(
+					`Request took too long! Timeout after ${s} second${
+						s === 1 ? "" : "s"
+					}`
+				)
+			);
+		}, s * 1000);
 	});
-});
+};
 
-answerButton.addEventListener("click", async function () {
-	const callId = callInput.value; // Get the call ID from your input field
-
-	// Create references to the Firestore collection and document
-	const callsCollection = collection(firestore, "calls");
-	const callDocRef = doc(callsCollection, callId); // Use the provided callId as the document ID
-
-	// Create a reference to the "answerCandidates" subcollection
-	const answerCandidates = collection(callDocRef, "answerCandidates");
-
-	pc.addEventListener("icecandidate", async function (e) {
-		if (e.candidate) {
-			try {
-				// Add the ICE candidate to Firestore's answerCandidates collection
-				await addDoc(answerCandidates, e.candidate.toJSON());
-			} catch (error) {
-				console.error("Error adding ICE candidate to Firestore:", error);
-			}
-		}
-	});
-
+const createCall = async function () {
 	try {
+		await openWebcam();
+		const callsCollection = collection(firestore, "calls");
+		const callDoc = doc(callsCollection);
+
+		const callDocRef = doc(callsCollection, callDoc.id);
+		const offerCandidates = collection(callDocRef, "offerCandidates");
+		const answerCandidates = collection(callDocRef, "answerCandidates");
+
+		if (!state.online) throwError("Error: Check Your Internet Connection 💥💥");
+		const link = window.location.href.split("#")[0];
+
+		copyTextToClipboard(`${link}#${callDocRef.id}`);
+
+		// Add icecandidate event listener
+		pc.addEventListener("icecandidate", async (event) => {
+			if (event.candidate) {
+				// Handling the ice candidate
+				await addDoc(offerCandidates, event.candidate.toJSON());
+			}
+		});
+
+		if (!state.online)
+			throw new Error("You're not connected to the internet. 💥💥");
+		const offerDescription = await Promise.race([
+			pc.createOffer(),
+			timeout(10),
+		]);
+		await pc.setLocalDescription(offerDescription);
+
+		const offer = {
+			sdp: offerDescription.sdp,
+			type: offerDescription.type,
+		};
+
+		// Store the offer in Firestore
+		await Promise.race([setDoc(callDocRef, { offer }), timeout(10)]); // Use setDoc to store the offer as an object
+
+		// Listen for changes in Firestore document
+		onSnapshot(callDocRef, (snapshot) => {
+			// Listen to callDocRef instead of callsCollection
+			const data = snapshot.data();
+			console.log(data);
+
+			if (!pc.currentRemoteDescription && data?.answer) {
+				const answerDescription = new RTCSessionDescription(data.answer);
+				pc.setRemoteDescription(answerDescription);
+			}
+
+			// Handle answerCandidates as you were doing
+			onSnapshot(answerCandidates, (snapshot) => {
+				snapshot.docChanges().forEach((change) => {
+					if (change.type === "added") {
+						const candidate = new RTCIceCandidate(change.doc.data());
+						pc.addIceCandidate(candidate);
+					}
+				});
+			});
+		});
+	} catch (err) {
+		showMessage(err.message);
+	}
+};
+
+const answerCall = async function (number = 12320) {
+	try {
+		await openWebcam();
+
+		const callId = window.location.href.split("#")[1] || number;
+		console.log(window.location.href.split("#")[1]);
+		console.log(number);
+		console.log(callId);
+
+		// Create references to the Firestore collection and document
+		const callsCollection = collection(firestore, "calls");
+		const callDocRef = doc(callsCollection, callId); // Use the provided callId as the document ID
+
+		// Create a reference to the "answerCandidates" subcollection
+		const answerCandidates = collection(callDocRef, "answerCandidates");
+
+		if (!state.online) throwError("You're not connected to the internet.");
+		pc.addEventListener("icecandidate", async function (e) {
+			if (e.candidate) {
+				try {
+					// Add the ICE candidate to Firestore's answerCandidates collection
+					await addDoc(answerCandidates, e.candidate.toJSON());
+				} catch (error) {
+					throwError("Error connecting you with caller");
+				}
+			}
+		});
+
 		const callDataSnapshot = await getDoc(callDocRef);
 		if (callDataSnapshot.exists()) {
 			const callData = callDataSnapshot.data();
-			console.log(callData);
 
 			const offerDescription = new RTCSessionDescription(callData.offer);
 			await pc.setRemoteDescription(offerDescription);
@@ -198,19 +239,131 @@ answerButton.addEventListener("click", async function () {
 				});
 			});
 		} else {
-			console.error("Document does not exist:", callId);
+			throw new Error("Call does not exist:", callId);
 		}
 	} catch (error) {
-		console.error("Error fetching document:", error);
+		// Handling error
+		showMessage(err.message);
 	}
-});
+};
 
-// Set pc.ontrack event handler outside the answerButton event listener
 pc.ontrack = (event) => {
-	console.log("Received remote tracks:", event.streams);
 	if (event.streams && event.streams[0]) {
 		remoteVideo.srcObject = event.streams[0];
-		console.log(remoteVideo);
 	}
-	console.log("Hello");
 };
+
+// Helper Function
+const checkInternetConnection = function () {
+	if (navigator.onLine) {
+		state.online = true;
+	} else {
+		state.online = false;
+	}
+};
+
+const throwError = function (errMessage) {
+	throw new Error(errMessage);
+};
+
+const copyTextToClipboard = function (text) {
+	if (navigator.clipboard) {
+		navigator.clipboard
+			.writeText(text)
+			.then(() => {
+				// Clipboard write succeeded
+				showNotification(
+					"Call Link Copied",
+					`The call link has been copied to the clipboard. Send it to the person you want to speak to.`,
+					text
+				);
+			})
+			.catch((error) => {
+				// Clipboard write failed, handle the error
+				console.error("Clipboard write failed:", error);
+				showMessage(`Your call link ${text}`);
+			});
+	} else {
+		// Fallback method for browsers that don't support Clipboard API
+		const textarea = document.createElement("textarea");
+		textarea.value = text;
+		document.body.appendChild(textarea);
+		textarea.select();
+		document.execCommand("copy");
+		document.body.removeChild(textarea);
+		showNotification(
+			"Call Link Copied",
+			`The call link has been copied to the clipboard. Send it to the person you want to speak to`,
+			text
+		);
+
+		showMessage(`${text}`);
+	}
+};
+
+const showMessage = function (message) {
+	tipMessage.textContent = message;
+	tipCont.classList.add("show");
+	setTimeout(() => {
+		tipCont.classList.remove("show");
+	}, 10000);
+};
+
+const showNotification = function (title, message, code) {
+	if ("Notification" in window) {
+		Notification.requestPermission().then(function (permission) {
+			if (permission === "granted") {
+				new Notification(title, {
+					body: message,
+				});
+			} else {
+				showMessage(`Call link copied to clipboard. (${code})`);
+			}
+		});
+	} else {
+		showMessage(`Call link copied to clipboard. ${code}`);
+	}
+};
+
+const changeCallHref = function () {
+	if (performance.navigation.type === 1) {
+		// Change the URL to a new location
+
+		window.location.href = "/";
+	}
+};
+
+const hangup = function () {
+	// Stop local video and audio streams
+	localStream = null;
+
+	remoteStream = null;
+	webCamVideo.srcObject = null;
+	remoteVideo.srcObject = null;
+
+	// Close the RTCPeerConnection (WebRTC)
+	pc.close();
+	pc = new RTCPeerConnection(servers);
+
+	// Inform the other participant(s) and perform cleanup
+	// Update UI to show call has ended
+};
+
+// Check internet connection when the page loads
+checkInternetConnection();
+
+window.addEventListener("online", checkInternetConnection);
+window.addEventListener("offline", checkInternetConnection);
+answerButton.addEventListener("click", function () {
+	const number = callInput.value.split("#")[1];
+	answerCall(number);
+});
+window.addEventListener("load", function () {
+	if (this.window.location.href.split("#")[1]?.length > 2) {
+		answerCall();
+	}
+});
+// webcamButton.addEventListener("click", openWebcam);
+callButton.addEventListener("click", createCall);
+hangupButton.addEventListener("click", hangup);
+changeCallHref();
